9ecc248c9937303f987afc8d21ebd34ce27a9cd8
unknown <jasonhassell228@gmail.com>
2019-06-24 14:20:59 +1000

    initial CLMoviesDB

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>@yield('title', 'CLMovieDB')</title>
+<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <h1 class="logo"><a href="/">CLMovieDB</a></h1>
+            <div class="search">
+                <input type="text" name="search" placeholder="Search for movies">
+                <button>
+                    <img src="/css/images/search.png" class="h-8 w-8 self-center">
+                </button>
+            </div>
+        </nav>
+    </header>
+    
+    @yield('content')
+
+    <footer>
+        <p> Molestias totam et quia. Architecto aut explicabo delectus odio odit veritatis. Atque aut illo quis. Modi tenetur alias sed nihil commodi reiciendis consequatur quaerat.  </p>
+    </footer>
+</body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'dark-grey': '#42454F',
+        'yellow-ish': '#E3E336',
+        'grey': '#5C5C5C',
+        'light-grey': '#535456',
+      },
+      maxWidth: {
+        '7xl': '80%',
+        '8xl': '83.333%',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
